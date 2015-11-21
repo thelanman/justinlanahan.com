fd03ebcc03e956d679d0d5ab7685e2d609eb252b
thelanman <lanahan.j@gmail.com>
2015-11-20 20:24:36 -0500

    cleaned up business logic and triggers for saving and deleting

--- life_log/admin.py
+++ life_log/admin.py
@@ -3,10 +3,16 @@ from django.contrib import admin
 from models import Event, Query
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('raw', 'event_dt', 'submitted_dt')
+    list_display = ('user', 'raw', 'event_dt', 'submitted_dt')
+    filter_horizontal = ('queries',)
+
+    def save_related(self, request, form, *args, **kwargs):
+        super(EventAdmin, self).save_related(request, form, *args, **kwargs)
+        obj = form.instance
+        obj.check_queries()
 
 class QueryAdmin(admin.ModelAdmin):
-    list_display = ('raw', 'f_raw', 'groupby', 'key', 'count', 'value')
+    list_display = ('user', 'raw', 'f_raw', 'groupby', 'key', 'count', 'value')
 
 admin.site.register(Event, EventAdmin)
 admin.site.register(Query, QueryAdmin)
--- life_log/models.py
+++ life_log/models.py
@@ -1,12 +1,17 @@
 import pickle
+import logging
 
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_delete, post_delete
 
 import lifelogger.parser
 import lifelogger.utils
+
+
+logger = logging.getLogger(__name__)
 
 
 class Query(models.Model):
@@ -15,7 +20,6 @@ class Query(models.Model):
     key = models.TextField(default='', blank=True)
     f_raw = models.TextField(blank=True, default='len')
     groupby = models.TextField(blank=True, default='')
-    q_pickled = models.TextField(blank=True, null=True)
     submitted_dt = models.DateTimeField(auto_now_add=True)
     modified_dt = models.DateTimeField(auto_now=True)
     count = models.IntegerField(default=0, blank=True)
@@ -23,16 +27,14 @@ class Query(models.Model):
 
     def clean(self):
         try:
-            q = lifelogger.parser.parse_query(self.raw)
+            self.q = lifelogger.parser.parse_query(self.raw)
         except Exception, e:
             raise ValidationError(str(e))
-        #self.q_pickled = pickle.dumps(q)
        
     @classmethod 
     def from_db(cls, db, field_names, values):
         instance = super(Query, cls).from_db(db, field_names, values)
         instance._loaded_values = dict(zip(field_names, values))
-        #instance.q = pickle.loads(instance._loaded_values['q_pickled'])
         instance.raw_init = instance._loaded_values['raw']
         instance.q = lifelogger.parser.parse_query(instance.raw_init)
         instance.key_init = instance._loaded_values['key']
@@ -40,30 +42,32 @@ class Query(models.Model):
         instance.groupby_init = instance._loaded_values['groupby']
         return instance
 
-    def calculate(self):
-        events_data = [e.data for e in self.event_set.objects.all()]
-        c, v = lifelogger.utils.calculate(events_data, self.q, self.k, self.f, self.groupby)
+    def calculate(self, event_data=None):
+        events_data = [e.data for e in self.event_set.all()] if event_data is None else event_data
+        c, v = lifelogger.utils.calculate(events_data, self.q, self.key, self.f_raw, self.groupby)
         self.count = c
         self.value = str(v)
 
     def check_events(self):
+        event_data = []
         for e in Event.objects.filter(user=self.user):
-           r =  self.q(e.data)
-           if r:
-               self.event_set.add(e)
+            r =  self.q(e.data)
+            if r:
+                self.event_set.add(e)
+                event_data += [e.data]
+        self.calculate(event_data)
 
     def update_events(self):
-        self.event_set.all().delete()
+        self.event_set.clear()
         self.check_events()
 
-    def save(self):
+    def save(self, *args, **kwargs):
         if self.pk is not None and (self.raw_init != self.raw or self.key_init != self.key or self.f_raw != self.f_raw_init or self.groupby_init != self.groupby):
             self.update_events()
         if self.pk is None:
-            super(Query, self).save()
-            self.q = lifelogger.parser.parse_query(self.raw)
+            super(Query, self).save(*args, **kwargs)
             self.check_events()
-        super(Query, self).save()
+        super(Query, self).save(*args, **kwargs)
         
     def __unicode__(self):
         return 'Query(%s %s: %s)' % (self.user, self.raw, self.f_raw)
@@ -71,10 +75,10 @@ class Query(models.Model):
     def __repr__(self):
         return 'Query(%s %s: %s)' % (self.user, self.raw, self.f_raw)
 
-
+        
 class Event(models.Model):
     user = models.ForeignKey(User)
-    queries = models.ManyToManyField(Query, blank=True)
+    queries = models.ManyToManyField(Query, null=True, blank=True)
     raw = models.TextField()
     data_pickled = models.TextField(blank=True, null=True)
     submitted_dt = models.DateTimeField(auto_now_add=True)
@@ -82,22 +86,24 @@ class Event(models.Model):
     modified_dt = models.DateTimeField(auto_now=True)
 
     def clean(self):
-        data = {}
         try:
-            data = lifelogger.parser.parse_event(self.raw, tz='America/New_York')
+            self.data = lifelogger.parser.parse_event(self.raw, tz='America/New_York')
         except Exception, e:
             raise ValidationError(str(e))
-        self.data_pickled = pickle.dumps(data)
+        self.event_dt = self.data.get('datetime', None)
+        self.data_pickled = pickle.dumps(self.data)
 
     def check_queries(self):
-        queries = self.queries.all()
+        queries = Query.objects.filter(user=self.user)
         for q in queries:
             r = q.q(self.data)
             if r:
-                q.event_set.add(self)
+                self.queries.add(q)
+                q.calculate()
+                q.save()
 
     def update_queries(self):
-        self.queries.all().delete()
+        self.queries.clear()
         self.check_queries()
 
     @classmethod
@@ -109,13 +115,13 @@ class Event(models.Model):
         instance.event_dt_init = instance._loaded_values['event_dt'] 
         return instance
 
-    def save(self):
+    def save(self, *args, **kwargs):
         if self.pk is not None and (self.raw_init != self.raw or self.event_dt_init != self.event_dt):
             self.update_queries()
         if self.pk is None:
-            super(Event, self).save()
+            super(Event, self).save(*args, **kwargs)
             self.check_queries()
-        super(Event, self).save()
+        super(Event, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return 'Event(%s: %s: %s)' % (self.user, self.event_dt, self.raw)
@@ -124,3 +130,12 @@ class Event(models.Model):
         return 'Event(%s: %s: %s)' % (self.user, self.event_dt, self.raw)
 
 
+def event_pre_delete(sender, *args, **kwargs):
+    instance = kwargs['instance']
+    queries = instance.queries.all()
+    for q in queries:
+        instance.queries.remove(q)
+        q.calculate()
+        q.save()
+
+pre_delete.connect(event_pre_delete, sender=Event)
